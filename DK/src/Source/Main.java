b053aa82729a891a18b90ae8bad6aa25a785b2c0
Elly Esparza <sparky.pizza.427@gmail.com>
2023-06-11 19:39:46 -0300

    C.SELECTOR_Main_V1.1.0

--- DK/src/Source/Main.java
+++ DK/src/Source/Main.java
@@ -35,6 +35,49 @@ public class Main {
         }
         //***************************************************************
 
+        //DRAWING THE CHARACTER SELECTOR **************************************
+        PennDraw.text(0.5, 0.7, "use 'w' to move up, 's' for right,");
+        PennDraw.text(0.5, 0.6, "'w' to jump, and 's' to move down.");
+        PennDraw.text(0.5, 0.5, "Climb the ladders to get to Princess Peach to win");
+        PennDraw.text(0.5, 0.4, "Avoid the barrels - you lose if one hits you");
+        PennDraw.text(0.5, 0.27, "Press 'f' to activate the special power up");
+        PennDraw.setFontBold();
+        PennDraw.text(0.5, 0.1, "Press 'y' to start the game");
+
+        //waits until user presses 'y' to begin the game
+        int selectPos = 0;
+        boolean selected = false;
+        while (!selected) {
+
+            PennDraw.clear();
+            PennDraw.text(0.5, 0.8, "Choose your character:");
+            PennDraw.text(0.5, 0.7, "Mario");
+            PennDraw.text(0.5, 0.6, "Pikachu");
+            PennDraw.text(0.35, 0.7 - 0.1 * selectPos, ">>");
+            PennDraw.text(0.5, 0.1, "Press 'y' to select");
+            while(true){
+                if (PennDraw.hasNextKeyTyped()) {
+                    c = PennDraw.nextKeyTyped();
+                    switch(c){
+                        case 'w':
+                            selectPos--;
+                            if(selectPos < 0) selectPos = 0;
+                            break;
+                        case 's':
+                            selectPos++;
+                            if(selectPos > 1) selectPos = 1;
+                            break;
+                        case 'y':
+                            selected = true;
+                            break;
+                    }
+                    break;
+                }
+            }
+        }
+        MarioFactory.SetCharacterOne(selectPos);
+        //***************************************************************
+
         //begin playing background music immediately
         StdAudio.loop("SFX/bacmusic.wav");
 
