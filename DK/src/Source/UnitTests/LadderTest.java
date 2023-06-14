bff327ec238d538c22ca261c10e317373e724f4f
w3rqil <128259227+w3rqil@users.noreply.github.com>
2023-06-13 23:30:49 -0300

    DEV_Tests_v1.0.1

---
+++ DK/src/Source/UnitTests/LadderTest.java
@@ -0,0 +1,10 @@
+package Source.UnitTests;
+import Source.Floor;
+import Source.Barrel;
+import Source.Mario;
+import org.junit.Test;
+import static org.junit.Assert;
+
+
+public class LadderTest {
+}
