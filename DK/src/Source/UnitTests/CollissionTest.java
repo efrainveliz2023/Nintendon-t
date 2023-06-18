fa54c75d680d4ea39c51507ecebe0abccdbd2a51
w3rqil <128259227+w3rqil@users.noreply.github.com>
2023-06-18 14:13:53 -0300

    CollisionTest_c.1.0.2

--- DK/src/Source/UnitTests/CollissionTest.java
+++ DK/src/Source/UnitTests/CollissionTest.java
@@ -5,40 +5,51 @@ import org.junit.Test;
 import org.junit.Assert;
 
 public class CollissionTest {
-    public static void main(String[] args){ runCollisionTests();}
+    //public static void main(String[] args){ runCollisionTests();}
 
-    @Test
-    public static void runCollisionTests(){
+    //@Test
+  //  public static void runCollisionTests(){
         // Mario collisions
         //Mario mario = new Mario(0.4, 0.6  + Mario.getHalfHeight());
+    @Test
+    public void marioCollisionTests() {
         Floor floor = new Floor(0.4, 0.65);
-        Barrel barrel = new Barrel(0.4, 0.6 + Floor.getHeight() +  0.025) ;
-        boolean collision = CollisionDetector.checkMarioCollision(floor.getX(), floor.getY(), 0.01,0.4);
+        Barrel barrel = new Barrel(0.4, 0.6 + Floor.getHeight() + 0.025);
+        boolean collision = CollisionDetector.checkMarioCollision(floor.getX(), floor.getY(), 0.01, 0.4);
         Assert.assertTrue("There's not been a collision with the floor", collision);
-        collision = CollisionDetector.checkMarioCollision(barrel.getX(), barrel.getY(), 0.025,0.025);
+        collision = CollisionDetector.checkMarioCollision(barrel.getX(), barrel.getY(), 0.025, 0.025);
         Assert.assertTrue("There's not been a collision with a barrel", collision);
-
+    }
+    @Test
+    public void closestFloorTest() {
         //getClosesFloorY
-        double closest= CollisionDetector.getCLosestFloorY(0.45);
-        boolean aux= closest == floor.getY();
+        Floor floor = new Floor(0.4, 0.65);
+        double closest = CollisionDetector.getCLosestFloorY(0.45);
+        boolean aux = closest == floor.getY();
         Assert.assertTrue("The closest was wrong", aux);
-        //Floor Collisions
+    }
+    //Floor Collisions
+    @Test
+    public void floorCollisionTests() {
         //Barril que colisiona con floor
-        Barrel barrel2 = new Barrel(0.4,0.65);
-        boolean collision2 = CollisionDetector.checkMarioCollision(barrel.getX(), barrel.getY(), 0.025, 0.025);
+        Barrel barrel2 = new Barrel(0.4, 0.65);
+        boolean collision2 = CollisionDetector.checkMarioCollision(barrel2.getX(), barrel2.getY(), 0.025, 0.025);
         Assert.assertTrue("There's not been a barrer-floor collision", collision2);
+    }
 
 
-
-        //Ladder collision
+    //Ladder collision
+    @Test
+    public void ladderCollisionTests() {
         Ladder ladder = new Ladder(0.4, 0.125);
         //mario que colisiona con ladder
         Mario mario2 = new Mario(0.4, 0.125);
-        boolean collision3= CollisionDetector.checkLaddersCollision(mario2.getX(), mario2.getY(), Mario.getHalfWidth());
+        boolean collision3 = CollisionDetector.checkLaddersCollision(mario2.getX(), mario2.getY(), Mario.getHalfWidth());
         Assert.assertTrue("There's not been a Mario-Ladder collision", collision3);
+    }
         
         //CollisionDetector.checkMarioCollision(peach.getX(), peach.getY(), 0.01, 0.015)
 
 
-    }
+//    }
 }
