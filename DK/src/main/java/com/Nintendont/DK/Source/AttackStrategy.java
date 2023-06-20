09da7512d0e1f5b175201fe695b21b9cdc7de16c
EllySp <128258356+EllySp@users.noreply.github.com>
2023-06-20 03:26:59 -0300

    Revert "src directory correction"
    This reverts commit 76d712b18d9b117bc0d03736018719a39475e680.

---
+++ DK/src/main/java/com/Nintendont/DK/Source/AttackStrategy.java
@@ -0,0 +1,9 @@
+package main.java.com.Nintendont.DK.Source;
+
+public interface AttackStrategy {
+    public void performAttack(Mario mario);
+
+    public void Run();
+
+    public void Clear();
+}
