f50674bf24e3ec49e30cfd0f7b05fb78d0f89bf5
EstebanMoroz <esteban.moroz@mi.unc.edu.ar>
2023-06-21 13:59:29 -0300

    test.v4

--- src/test/java/com.Nintendont.DK/Test/CollisionTest.java
+++ src/test/java/com.Nintendont.DK/Test/CollisionTest.java
@@ -3,14 +3,15 @@ import com.Nintendont.DK.Source.*;
 import org.junit.Test;
 import org.junit.Assert;
 public class CollisionTest {
-    Floor[] floors;
+    protected Floor[] floors;
     Ladder[] ladders;
     @Test
     public void runCollisionTests(){
-        Floor floor = new Floor(0.4, 0.6);
+        floors = new Floor[1];
         Barrel barrel = new Barrel(0.2, 0.6 + Floor.getHeight() +  0.025) ;
+            floors[0]=new Floor(0.4, 0.6);;
 
-        floors[0]=floor;
+
 
 
         //mario is collissioning to the barel
@@ -25,11 +26,15 @@ public class CollisionTest {
     }
     @Test
     public void runLadderTest(){
-        Ladder lader=new Ladder(0.4,0.6);
-        ladders[0]=lader;
+
+
+        ladders = new Ladder[1];
+        ladders[0]=new Ladder(0.4, 0.6);;
+
+
         CollisionDetector collision = new CollisionDetector();
         collision.setLadders(ladders);
-        Mario mario1 = new Mario(0.4, 0.6 + Floor.getHeight() + Mario.getHalfHeight());
+        Mario mario1 = new Mario(0.4, 0.5 + Floor.getHeight() + Mario.getHalfHeight());
         collision.setMario(mario1);
         boolean ladderTest = CollisionDetector.checkLaddersCollision(mario1.getX(), mario1.getY(), 0.025);
         Assert.assertTrue("Mario no esta en escalera", ladderTest);
