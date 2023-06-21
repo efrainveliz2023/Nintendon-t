386dac29ed4bffe2c948782dd3c2b962a50d7c99
EstebanMoroz <esteban.moroz@mi.unc.edu.ar>
2023-06-21 13:37:33 -0300

    Tests/master/colision

--- src/test/java/com.Nintendont.DK/Test/CollisionTest.java
+++ src/test/java/com.Nintendont.DK/Test/CollisionTest.java
@@ -1,24 +1,29 @@
 package com.Nintendont.DK.Test;
-import com.Nintendont.DK.Source.CollisionDetector;
-import com.Nintendont.DK.Source.Floor;
-import com.Nintendont.DK.Source.Barrel;
-import com.Nintendont.DK.Source.Mario;
+import com.Nintendont.DK.Source.*;
 import org.junit.Test;
 import org.junit.Assert;
 public class CollisionTest {
     @Test
-    public void runBarrelTests(){
+    public void runCollisionTests(){
         Floor floor = new Floor(0.4, 0.6);
         Barrel barrel = new Barrel(0.2, 0.6 + Floor.getHeight() +  0.025) ;
 
+
         //mario is collissioning to the barel
         Mario mario = new Mario(0.4, 0.6 + Floor.getHeight() + Mario.getHalfHeight());
-        //mario is not collisionning
-        Mario mario1 = new Mario(0.7, 0.6 + Floor.getHeight() + Mario.getHalfHeight());
-        CollisionDetector colision=new CollisionDetector();
 
 
+        boolean floors = CollisionDetector.checkFloorsCollision(mario.getX(), mario.getY(), 0.025);
+        Assert.assertTrue("Mario no esta en el piso", floors);
 
+
+    }
+    @Test
+    public void runLadderTest(){
+        Ladder lader=new Ladder(0.4,0.6);
+        Mario mario1 = new Mario(0.4, 0.6 + Floor.getHeight() + Mario.getHalfHeight());
+        boolean ladder = CollisionDetector.checkLaddersCollision(mario1.getX(), mario1.getY(), 0.025);
+        Assert.assertTrue("Mario no esta en escalera", ladder);
     }
 
 }
